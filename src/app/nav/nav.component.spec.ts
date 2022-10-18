ddec5bc2d75a8366ac2f0fc04a38c5fe07f1235c
Roberto Duran <shaggyrd+github@gmail.com>
2022-10-18 08:25:04 -0500

    add unit testing and one e2e test

---
+++ src/app/nav/nav.component.spec.ts
@@ -0,0 +1,46 @@
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {of} from 'rxjs';
+import { RouterTestingModule } from "@angular/router/testing";
+import { By } from "@angular/platform-browser";
+import {NavComponent} from './nav.component';
+import { AuthService } from "../services/auth.service";
+
+describe('NavComponent', () => {
+    let component: NavComponent;
+    let fixture: ComponentFixture<NavComponent>;
+    const mockedAuthService = jasmine.createSpyObj(
+        'AuthService',
+        ['createUser', 'logout'],
+        {IsAuthenticated$: of(true),});
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [NavComponent],
+            imports: [RouterTestingModule],
+            providers: [
+                { provide: AuthService, useValue: mockedAuthService }
+            ]
+        })
+            .compileComponents();
+
+        fixture = TestBed.createComponent(NavComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should Logout', () => {
+        const logoutLink = fixture.debugElement.query(By.css('li:nth-child(3) a'));
+        expect(logoutLink).withContext('Not Log in').toBeTruthy();
+
+        logoutLink.triggerEventHandler('click');
+
+        const service = TestBed.inject(AuthService);
+        expect(service.logout)
+            .withContext('Could not click logout link')
+            .toHaveBeenCalledTimes(1);
+    });
+});
